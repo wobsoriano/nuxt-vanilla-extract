609d118fd4f76204f395c3c485be6e36571071e4
Robert Soriano <sorianorobertc@gmail.com>
2022-06-19 05:32:40 -0700

    add playground

---
+++ playground/nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt'
+import Module from '../src/module'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [Module],
+})
